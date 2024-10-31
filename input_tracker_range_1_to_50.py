5f25a28f3691afed88c1e2a399cc4d709ac188fb
LianKarylGarcia <liankarylg@gmail.com>
2024-10-31 01:26:03 -0400

    Pseudocode

---
+++ input_tracker_range_1_to_50.py
@@ -0,0 +1,6 @@
+# Ask the user for input.
+# Set a valid number range from 1 to 50.
+# Next, assign ranges: Range 1 for 1–10, Range 2 for 11–20, Range 3 for 21–30, Range 4 for 31–40. Range 5 for 41–50
+# Set a count for all ranges to 0.
+# Count each user input, adding it to the respective range based on the number they input.
+# If the user input is invalid, print "Invalid input" and display the results.
