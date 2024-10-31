dff32621923f7047c82a80fe705f7abd306fe4ae
LianKarylGarcia <liankarylg@gmail.com>
2024-10-31 01:40:37 -0400

    Made some few changes to the arrangement of ranges

--- input_tracker_range_1_to_50.py
+++ input_tracker_range_1_to_50.py
@@ -1,5 +1,11 @@
-# Set a valid number range from 1 to 50.
-ranges = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
+# Next, assign ranges: Range 1 for 1–10, Range 2 for 11–20, Range 3 for 21–30, Range 4 for 31–40. Range 5 for 41–50
+range_1 = 1 - 10
+range_2 = 11 - 20
+range_3 = 21 - 30
+range_4 = 31 - 40
+range_5 = 41 - 50
+
+ranges = {range_1: 0, range_2: 0, range_3: 0, range_4: 0, range_5: 0}
 
 # Ask the user for input.
 while True: # using loop
@@ -11,7 +17,7 @@ while True: # using loop
 
 
 
-# Next, assign ranges: Range 1 for 1–10, Range 2 for 11–20, Range 3 for 21–30, Range 4 for 31–40. Range 5 for 41–50
+# Set a valid number range from 1 to 50.
 # Set a count for all ranges to 0.
 # Count each user input, adding it to the respective range based on the number they input.
 # If the user input is invalid, print "Invalid input" and display the results.
