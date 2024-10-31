b360436416d79e921c0b3d2ad2397e7cc01e46cd
LianKarylGarcia <liankarylg@gmail.com>
2024-10-31 01:33:04 -0400

    Can successfully prompt the user to enter a number between 1 and 50.

--- input_tracker_range_1_to_50.py
+++ input_tracker_range_1_to_50.py
@@ -1,5 +1,16 @@
-# Ask the user for input.
 # Set a valid number range from 1 to 50.
+ranges = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0}
+
+# Ask the user for input.
+while True: # using loop
+    try: # using try and except incase of an error
+        num = int(input("Please enter a number between 1 and 50: "))
+    except ValuError:
+        break
+    
+
+
+
 # Next, assign ranges: Range 1 for 1–10, Range 2 for 11–20, Range 3 for 21–30, Range 4 for 31–40. Range 5 for 41–50
 # Set a count for all ranges to 0.
 # Count each user input, adding it to the respective range based on the number they input.
